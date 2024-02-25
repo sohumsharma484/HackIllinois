730c32517bd9d6682b0c2a3c6a28352889bc978d
Andrew Chen <superandrewchen11@gmail.com>
2024-02-24 22:54:49 -0600

    Unity Dev

--- program/programs/lumberjack/src/lib.rs
+++ program/programs/lumberjack/src/lib.rs
@@ -7,7 +7,7 @@ use anchor_lang::InstructionData;
 use instructions::*;
 pub mod instructions;
 
-declare_id!("FraPqyV8kpJuezPRGUmJLKfZfggQz2wWvGznHFGpZ3pw");
+declare_id!("HN3ujuEqqAWCwiUR7q9xgKUv2tNqVYaeSKeTPs2e5pHv");
 
 #[error_code]
 pub enum GameErrorCode {
